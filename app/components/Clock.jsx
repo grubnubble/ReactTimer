646126712a20495540d5f0e9a586e35acdff3bcf
grubnubble <montanalisagoodman@gmail.com>
2017-01-20 13:21:30 -0500

    initial commit

---
+++ app/components/Clock.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Clock = (props) => {
+	return (
+		<div>
+			<p Clock.jsx rendered</p>
+			
+		</div>
+	);
+};
+
+module.exports = Clock;
---
+++ app/components/Controls.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Controls = (props) => {
+	return (
+		<div>
+			<p Controls.jsx rendered</p>
+			
+		</div>
+	);
+};
+
+module.exports = Controls;
