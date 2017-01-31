17238b62d25fe62673734c15115fb127a3b5a457
grubnubble <montanalisagoodman@gmail.com>
2017-01-31 16:09:31 -0500

    Add static Controls component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,12 +1,26 @@
 var React = require('react');
 
-var Controls = (props) => {
-	return (
-		<div>
-			<p>Controls.jsx rendered</p>
-			
-		</div>
-	);
-};
+var Controls = React.createClass({
+  propTypes: {
+    countdownStatus: React.PropTypes.string.isRequired
+  },
+  render: function () {
+    var {countdownStatus} = this.props;
+    var renderStartStopButton = () => {
+      if (countdownStatus === 'started') {
+        return <button className="button secondary">Pause</button>
+      } else if (countdownStatus === 'paused') {
+        return <button className="button primary">Start</button>
+      }
+    };
+
+    return (
+      <div className="controls">
+        {renderStartStopButton()}
+        <button className="button alert hollow">Clear</button>
+      </div>
+    )
+  }
+});
 
 module.exports = Controls;
